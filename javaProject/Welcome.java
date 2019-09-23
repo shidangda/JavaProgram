f731e30df22230e1ff66bfee8f9b7f5c908c6e61
unknown <2425200635@qq.com>
2019-09-23 20:56:04 +0800

    - t复  -m 复习题答案

--- javaProject/Welcome.java
+++ javaProject/Welcome.java
@@ -4,6 +4,6 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!  »¶Ó­Ñ§Ï°Java!");
 	}
 }
